3bbc2269fd37ff3af1f7af6067eaea9b4e982bf3
Andy Agus <redagus@gmail.com>
2020-05-18 15:06:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,7 +8,7 @@ def meal_choice(veg1, veg2, protein = 'tofu')
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
-puts meal
-return meal
+puts meal_choice
+return meal_choice
 end
 
